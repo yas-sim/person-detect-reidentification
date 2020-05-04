f03d0acd8ae6206f9c81c668e84d233a514e4287
Yasunori Shimura <63567854+yas-sim@users.noreply.github.com>
2020-05-04 13:13:48 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,79 @@
+# Person / Face detection and Re-identification Demo
+This is a demo program to demonstrate how person or face detection DL model and re-identification model works with Intel(r) Distribution of OpenVINO(tm) toolkit.  
+This program find the objects such as person or face from multiple images, and then assign ID and match objects in the pictures.
+The demo program suppors multiple camera or movie file inputs (The progra should work with more than 2 inputs, haven't tested though).  
+The re-identification model takes a cropped image of the object and generates a feature vector consists of 256 FP values. This program calculates the cosine distance of those feature vectores of the objects to check the object similarity.  
+The found objects are registered to a database with the created time. The time in the recored will be updated everytime the record is used so that the program can check the elapsed time from the last use. The record will be evicted when the specified time get passed.  
+
+[**Intel(r) Distribution of OpenVINO(tm) toolkit**](https://software.intel.com/en-us/openvino-toolkit)を使った人・顔検出＋マッチングデモプログラムです。  
+人検出、顔検出DLモデルを使用して複数の画像から検出したオブジェクトに、re-identificationモデルを使用してマッチング、ID振りを行っています。  
+2つ以上のカメラ、あるいはムービーをサポートすることも可能です（テストしてませんが）  
+Re-identificationモデルは切り出されたオブジェクトのイメージから256個のFP数値からなる特徴ベクトルを生成します。このプログラムでは生成されたベクトル同士のコサイン距離の比較によってオブジェクトの類似度を判定しています。  
+見つかったオブジェクトは時刻とともにデータベースに記録されます。レコードの時間はそのレコードが使用されるたびに更新され、最後に使用された時間がわかるようになっています。使用されないまま指定された時間が経過したオブジェクトはデータベースから除外されます。
+
+![Detection and Re-ID](./resources/reid.gif)  
+
+
+### Required DL Models to Run This Demo
+
+The demo expects the following models in the Intermediate Representation (IR) format:
+
+ * For person / pedestrian detection and re-identification
+   * `pedestrian-detection-adas-0002`
+   * `person-reidentification-retail-0079`
+
+ * For face detection and re-identification 
+   * `face-detection-adas-0001`
+   * `face-reidentification-retail-0095`
+
+You can download those models from OpenVINO [Open Model Zoo](https://github.com/opencv/open_model_zoo).
+In the `models.lst` is the list of appropriate models for this demo that can be obtained via `Model downloader`.
+Please see more information about `Model downloader` [here](../../../tools/downloader/README.md).
+
+## How to Run
+
+(Assuming you have successfully installed and setup OpenVINO 2020.2. If you haven't, go to the OpenVINO web page and follow the [*Get Started*](https://software.intel.com/en-us/openvino-toolkit/documentation/get-started) guide to do it.)  
+
+### 1. Install dependencies  
+The demo depends on:
+- `opencv-python`
+- `numpy`
+- `scipy`
+- `munkres` (for Hangarian combinational optimization method)
+
+To install all the required Python modules you can use:
+
+``` sh
+(Linux) pip3 install -r requirements.txt
+(Win10) pip install -r requirements.txt
+```
+
+### 2. Download DL models from OMZ
+Use `Model Downloader` to download the required models.
+``` sh
+(Linux) python3 $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/downloader.py --list models.lst
+(Win10) python "%INTEL_OPENVINO_DIR%\deployment_tools\tools\model_downloader\downloader.py" --list models.lst
+```
+
+### 3. Run the demo app
+This program doesn't take any command line arguments. All file names and paths are hard coded in the source code.
+``` sh
+(Linux) python3 person-detect-reid.py
+(Win10) python person-detect-reid.py
+```
+
+In default, the program assumes that 2 USB webCams are attached to your PC. If you want to use movie files instead, modify the source code (it's easy :-) )
+
+
+## Demo Output  
+The application draws the boundinx boxes and ID numbers on the images.  
+
+## Tested Environment  
+- Windows 10 x64 1909 and Ubuntu 18.04 LTS  
+- Intel(r) Distribution of OpenVINO(tm) toolkit 2020.2  
+- Python 3.6.5 x64  
+
+## See Also  
+* [Using Open Model Zoo demos](../../README.md)  
+* [Model Optimizer](https://docs.openvinotoolkit.org/latest/_docs_MO_DG_Deep_Learning_Model_Optimizer_DevGuide.html)  
+* [Model Downloader](../../../tools/downloader/README.md)  
---
+++ person-detect-reid.py
@@ -0,0 +1,159 @@
+import sys
+import math
+import time
+
+import cv2
+import numpy as np
+from scipy.spatial import distance
+from munkres import Munkres
+from openvino.inference_engine import IENetwork, IECore
+
+# DL models for pedestrian detection and person re-identification
+#model_det  = 'pedestrian-detection-adas-0002'
+#model_reid = 'person-reidentification-retail-0079'
+
+# DL models for face detection and re-identification
+model_det  = 'face-detection-adas-0001'
+model_reid = 'face-reidentification-retail-0095'
+
+model_det  = 'intel/' + model_det  + '/FP16/' + model_det
+model_reid = 'intel/' + model_reid + '/FP16/' + model_reid
+
+_N = 0
+_C = 1
+_H = 2
+_W = 3
+
+#video_caputure_list = [ 0, 1 ]
+video_caputure_list = [ 'movie1.264', 'movie2.264' ]   # for testing and debugging purpose
+
+def main():
+        id_num = 0
+        dist_threshold = 1.0
+        timeout_threshold = 5   # Object feature database timeout (sec)
+        feature_db = []         # Object feature database (feature, id)
+
+        ie = IECore()
+
+        # Prep for face/pedestrian detection
+        net_det  = ie.read_network(model_det+'.xml', model_det+'.bin')   # model=pedestrian-detection-adas-0002
+        input_name_det  = next(iter(net_det.inputs))                     # Input blob name "data"
+        input_shape_det = net_det.inputs[input_name_det].shape           # [1,3,384,672]
+        out_name_det    = next(iter(net_det.outputs))                    # Output blob name "detection_out"
+        out_shape_det   = net_det.outputs[out_name_det].shape            # [ image_id, label, conf, xmin, ymin, xmax, ymax ]
+        exec_net_det    = ie.load_network(net_det, 'CPU')
+
+        # Preparation for face/pedestrian re-identification
+        net_reid = ie.read_network(model_reid+".xml", model_reid+".bin") # person-reidentificaton-retail-0079
+        input_name_reid  = next(iter(net_reid.inputs))                   # Input blob name "data"
+        input_shape_reid = net_reid.inputs[input_name_reid].shape        # [1,3,160,64]
+        out_name_reid    = next(iter(net_reid.outputs))                  # Output blob name "embd/dim_red/conv"
+        out_shape_reid   = net_reid.outputs[out_name_reid].shape         # [1,256,1,1]
+        exec_net_reid    = ie.load_network(net_reid, 'CPU')
+
+        # Open USB webcams
+        num_cameras = 2
+        caps = [cv2.VideoCapture(vcap) for vcap in video_caputure_list]
+
+        while cv2.waitKey(1)!=27:                                 # 27 == ESC
+
+            objects = [[] for i in range(num_cameras)]
+
+            frames = [cap.read() for cap in caps]            # cv2.VideoCapture.read() returns [ ret, image]. Take only the image.
+            images = [ frame[1] for frame in frames]
+            for frame in frames:
+                if frame[0]==False:
+                    return
+
+            for cam in range(num_cameras):
+                in_frame = cv2.resize(images[cam], (input_shape_det[_W], input_shape_det[_H]))
+                in_frame = in_frame.transpose((2, 0, 1))
+                in_frame = in_frame.reshape(input_shape_det)
+                res_det = exec_net_det.infer(inputs={input_name_det: in_frame})     # Detect objects (pedestrian or face)
+
+                for obj in res_det[out_name_det][0][0]:           # obj = [ image_id, label, conf, xmin, ymin, xmax, ymax ]
+                    if obj[2] > 0.6:                              # Confidence > 60% 
+                        frame = images[cam]
+                        xmin = abs(int(obj[3] * frame.shape[1]))
+                        ymin = abs(int(obj[4] * frame.shape[0]))
+                        xmax = abs(int(obj[5] * frame.shape[1]))
+                        ymax = abs(int(obj[6] * frame.shape[0]))
+                        class_id = int(obj[1])
+
+                        # Obtain feature vector of the detected object using re-identification model
+                        obj_img=frame[ymin:ymax,xmin:xmax]                                           # Crop the found object
+                        obj_img=cv2.resize(obj_img, (input_shape_reid[_W], input_shape_reid[_H]))
+                        obj_img=obj_img.transpose((2,0,1))
+                        obj_img=obj_img.reshape(input_shape_reid)
+                        res_reid = exec_net_reid.infer(inputs={input_name_reid: obj_img})            # Run re-identification model to generate feature vectors (256 elem)
+                        
+                        vec=np.array(res_reid[out_name_reid]).reshape((256))                         # Convert the feature vector to numpy array
+                        objects[cam].append({'coord':[xmin,ymin, xmax,ymax], 'feature':vec, 'id':-1})
+
+            total_objects=0
+            for obj in objects:
+                total_objects += len(obj)
+            if total_objects ==0:
+                for i in range(num_cameras):
+                    cv2.imshow('cam'+str(i), images[i])
+                continue
+
+            # Check if the detected objects have already registered in the DB
+            for cam in range(num_cameras):
+                for idx_obj, obj in enumerate(objects[cam]):
+                    min_dist=1e9
+                    min_id=-1
+                    for idx_db ,db in enumerate(feature_db):             # Search for the most similar object in the DB
+                        dist = distance.cosine(obj['feature'], db['feature'])
+                        if dist<min_dist:
+                            min_dist   = dist
+                            min_id     = db['id']
+                            min_idx_db = idx_db
+                    if min_id != -1 and min_dist < dist_threshold:  # if the distance is smaller than the threshold, assign the ID to the object
+                        feature_db[min_idx_db]['time'] = time.time()     # Renew the last used time (extend lifetime of the DB record)
+                        objects[cam][idx_obj]['id'] = min_id
+
+            # Create cosine distance matrix and match objects in the frame and the DB
+            hangarian = Munkres()
+            for cam in range(num_cameras):
+                if len(feature_db)==0 or len(objects[cam])==0: continue
+                dist_matrix = [ [ distance.cosine(obj_db['feature'], obj_cam['feature']) 
+                            for obj_db in feature_db ] for obj_cam in objects[cam] ]
+                combination = hangarian.compute(dist_matrix)        # Solve matching problem
+
+                for idx_obj, idx_db in combination:
+                    if objects[cam][idx_obj]['id']!=-1:             # This object has already been assigned an ID
+                        continue
+                    dist = distance.cosine(objects[cam][idx_obj]['feature'], feature_db[idx_db]['feature'])
+                    if dist < dist_threshold:
+                        feature_db[idx_db]['time'] = time.time()    # Renew the last used time (extend lifetime of the DB record)
+                        objects[cam][idx_obj]['id'] = feature_db[idx_db]['id']
+            del hangarian
+
+            # Register remaining ID unassigned objects to the DB (They are considered as the new objects)
+            for cam in range(num_cameras):
+                for obj in objects[cam]:
+                    if obj['id'] == -1:
+                        feature_db.append({'id':id_num, 'feature':obj['feature'], 'time': time.time()})
+                        id_num+=1
+
+            # Check for timeout items in the DB and delete them
+            for i, db in enumerate(feature_db):
+                if time.time() - db['time'] > timeout_threshold:
+                    print('discarded id #{}'.format(db['id']))
+                    feature_db.pop(i)
+
+            # Draw bounding boxes and IDs
+            for camera in range(num_cameras):
+                for obj in objects[camera]:
+                    id = obj["id"]
+                    color = ( (((~id)<<6) & 0x100)-1, (((~id)<<7) & 0x0100)-1, (((~id)<<8) & 0x0100)-1 )
+                    xmin, ymin, xmax, ymax = obj['coord']
+                    cv2.rectangle(images[camera], (xmin, ymin), (xmax, ymax), color, 2)
+                    cv2.putText(images[camera], str(id), (xmin, ymin - 7), cv2.FONT_HERSHEY_COMPLEX, 1.0, color, 1)
+                cv2.imshow('cam'+str(camera), images[camera])
+
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+        sys.exit(main() or 0)
